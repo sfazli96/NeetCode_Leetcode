eebb3aed31783355e473964193166c008d86b381
sfazli96 <sfazli96@gmail.com>
2023-07-10 20:09:31 -0700

    subsets leetcode

---
+++ javascript/subset.js
@@ -0,0 +1,18 @@
+var subsets = function(nums) {
+    if (nums.length === 0) {
+        return [[]]
+    }
+
+    let num = nums[0]
+
+    let sliced = subsets(nums.slice(1))
+
+    let newArr = []
+
+    for (let subset of sliced) {
+        newArr.push([num].concat(subset));
+        newArr.push(subset);
+    }
+
+    return newArr;
+}
