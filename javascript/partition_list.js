2a6500102d9bcf65f18ba640da8709db40105d34
sfazli96 <sfazli96@gmail.com>
2023-08-23 17:12:35 -0700

    rotate array and partition list

---
+++ javascript/partition_list.js
@@ -0,0 +1,24 @@
+var partition = function(head, x) {
+    let current = head;
+    let smallX = new ListNode(); // Create a new node here
+    let bigX = new ListNode();   // Create a new node here
+    let smallTail = smallX;
+    let bigTail = bigX;
+
+    while (current !== null) {
+        let newNode = new ListNode(current.val); // Create a new node for current value
+        if (current.val < x) {
+            smallTail.next = newNode; // Use the new node here
+            smallTail = newNode;
+        } else {
+            bigTail.next = newNode; // Use the new node here
+            bigTail = newNode;
+        }
+
+        current = current.next;
+    }
+
+    bigTail.next = null;
+    smallTail.next = bigX.next;
+    return smallX.next;
+};
---
+++ javascript/rotate_array.js
@@ -0,0 +1,24 @@
+var rotate = function(nums, k) {
+    const n = nums.length;
+    k = k % n;
+
+    // reverse array
+    reverse(nums, 0, n - 1)
+
+    // Reverse the first k elements
+    reverse(nums, 0, k - 1)
+
+    reverse(nums, k, n - 1)
+};
+
+function reverse(nums, start, end) {
+    while (start < end) {
+        const tmp = nums[start]
+        nums[start] = nums[end]
+        nums[end] = tmp
+        start++
+        end--
+    }
+}
+
+
