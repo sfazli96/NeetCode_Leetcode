aa898b84b0fc56ab850747009ca6a44621e968fc
sfazli96 <sfazli96@gmail.com>
2023-09-15 23:54:54 -0700

    LC valid anagram in python

---
+++ python/valid_anagram.py
@@ -0,0 +1,24 @@
+def isAnagram(s, t):
+    if len(s) != len(t): # check if the strings are different
+        return False
+
+    s_count = {} # dictionaries
+    t_count = {}
+
+    for char in s:
+        if char in s_count:
+            s_count[char] += 1
+        else:
+            s_count[char] = 1
+
+    for char in t:
+        if char in t_count:
+            t_count[char] += 1
+        else:
+            t_count[char] = 1
+
+    return s_count == t_count
+
+# Test cases
+print(isAnagram("anagram", "nagaram"))  # True
+print(isAnagram("rat", "car"))          # False
