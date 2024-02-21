5d93d590e136d01691877923840352fac7e7f80e
sfazli96 <sfazli96@gmail.com>
2024-02-20 23:41:13 -0800

    missing number leetcode

---
+++ javascript/missing_number.js
@@ -0,0 +1,13 @@
+var missingNumber = function(nums) {
+    let n = nums.length;
+    let temp = new Array(n + 1).fill(0);
+    for (let i = 0; i < n; i++) {
+        let num = nums[i];
+        temp[num] = 1;
+    }
+    for (let i = 0; i < temp.length; i++) {
+        if (temp[i] === 0) {
+            return i;
+        }
+    }
+};
