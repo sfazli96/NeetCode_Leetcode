fff1cd35810372e11dd42b6c83a880c340b91a95
sfazli96 <sfazli96@gmail.com>
2024-03-27 20:53:38 -0700

    pascal 2 triangle

---
+++ javascript/pascal2.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    let row = new Array(rowIndex + 1).fill(1);
+
+    for (let i = 1; i <= rowIndex; i++) {
+        for (let j = i - 1; j > 0; j--) {
+            row[j] += row[j - 1];
+        }
+    }
+
+    return row;
+};
