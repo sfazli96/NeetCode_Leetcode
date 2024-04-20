064f927b248d911bdbc275e24a9bc06bfff04e93
Sameh Fazli <43709736+sfazli96@users.noreply.github.com>
2024-04-19 21:44:55 -0700

    Update var to let two_sum.js

--- javascript/two_sum.js
+++ javascript/two_sum.js
@@ -32,7 +32,7 @@ Output: [0,1]
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+let twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
