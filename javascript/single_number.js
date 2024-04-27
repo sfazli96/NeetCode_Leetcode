0366435d76faeb7a7316c9b2d7482b2ba060f1f1
Sameh Fazli <43709736+sfazli96@users.noreply.github.com>
2024-04-26 20:02:15 -0700

    Update var to llet single_number.js

--- javascript/single_number.js
+++ javascript/single_number.js
@@ -28,7 +28,7 @@ Output: 1
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
+let singleNumber = function(nums) {
     let result = 0
     for (let i = 0; i < nums.length; i++) {
         result ^= nums[i]
