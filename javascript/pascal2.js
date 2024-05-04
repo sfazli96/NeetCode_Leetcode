72615ce942509084f96e3f22e3731fea170aabe5
Sameh Fazli <43709736+sfazli96@users.noreply.github.com>
2024-05-04 10:59:02 -0700

    Updated var to let pascal2.js

--- javascript/pascal2.js
+++ javascript/pascal2.js
@@ -2,7 +2,7 @@
  * @param {number} rowIndex
  * @return {number[]}
  */
-var getRow = function(rowIndex) {
+let getRow = function(rowIndex) {
     let row = new Array(rowIndex + 1).fill(1);
 
     for (let i = 1; i <= rowIndex; i++) {
