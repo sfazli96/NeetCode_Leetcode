c1480fddcb2d7e516ad602f3be2d34b5d5a47c14
Sameh Fazli <43709736+sfazli96@users.noreply.github.com>
2024-06-14 00:03:24 -0700

    Update prefix_tree.py

--- python/prefix_tree.py
+++ python/prefix_tree.py
@@ -44,9 +44,3 @@ class Trie:
             return True
         else:
             return False
-
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
