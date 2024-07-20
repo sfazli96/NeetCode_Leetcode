7799a9dc397c406963fa9000a9651787606a742a
Sameh Fazli <43709736+sfazli96@users.noreply.github.com>
2024-07-19 21:07:17 -0700

    Update sellings_price.sql, wrote less lines

--- other/sellings_price.sql
+++ other/sellings_price.sql
@@ -1,11 +1,5 @@
-SELECT
-    us.product_id,
-    ROUND(SUM(p.price * us.units) / SUM(us.units), 2) AS average_price
-FROM
-    UnitsSold us
-JOIN
-    Prices p ON us.product_id = p.product_id
-WHERE
-    us.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY
-    us.product_id;
+SELECT us.product_id, ROUND(SUM(p.price * us.units) / SUM(us.units), 2) AS average_price
+FROM UnitsSold us
+JOIN Prices p ON us.product_id = p.product_id
+WHERE us.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY us.product_id;
