f772251d0d091523c95f01890ffa16f8b90a5dc3
Sameh Fazli <43709736+sfazli96@users.noreply.github.com>
2024-07-26 10:20:20 -0700

    Update largest_divisible.py, fixed spacing

--- python/largest_divisible.py
+++ python/largest_divisible.py
@@ -2,16 +2,13 @@ class Solution:
     def largestDivisibleSubset(self, nums):
         if not nums:
             return []
-
         nums.sort()
         dp = [[num] for num in nums]  
         max_subset = []
-
         for i in range(len(nums)):
             for j in range(i):
                 if nums[i] % nums[j] == 0 and len(dp[i]) < len(dp[j]) + 1:
                     dp[i] = dp[j] + [nums[i]]  # Extend the subset with current number
             if len(dp[i]) > len(max_subset):
                 max_subset = dp[i]
-
         return max_subset
