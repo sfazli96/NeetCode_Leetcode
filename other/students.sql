fdc8711a5f885914421697fd6dc2f92741457676
Sameh Fazli <43709736+sfazli96@users.noreply.github.com>
2024-07-26 10:12:14 -0700

    Update students.sql, fixed alignment

--- other/students.sql
+++ other/students.sql
@@ -1,7 +1,5 @@
-SELECT s.student_id,
-       s.student_name,
-       sub.subject_name,
-       COUNT(e.student_id) AS attended_exams
+SELECT s.student_id, s.student_name, sub.subject_name,
+COUNT(e.student_id) AS attended_exams
 FROM Students s
 CROSS JOIN Subjects sub
 LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
